427cfc7d42431deef63baaff702423d1179cd68e
zerg26 <eric.misiak.26@gmail.com>
2025-03-26 14:30:26 -0400

    better?

--- UNCCtutor/UNCCtutor/base/views.py
+++ UNCCtutor/UNCCtutor/base/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 from django.db.models import Q
+from django.contrib.auth.models import User
 
 
 # Create your views here.
 
 def loginPage(request):
+
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POSt.get('password')
+
+        try:
+            user =User.objects.get(email=email)
+        except:
+            messages.error(request, 'incorrect uncc email')
+    
     context = {}
     return render(request, 'base/login.html', context)
