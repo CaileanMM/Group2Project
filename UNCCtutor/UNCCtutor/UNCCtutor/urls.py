82c1b002ed7583019c2c6251e6f5d2ff6d6c04c8
Cailean Matthews <matthewsmcailean@gmail.com>
2025-03-31 18:20:17 -0400

    Fixed Fie Structure
    Fixed templates location and added UNCCtutor urls

--- UNCCtutor/UNCCtutor/UNCCtutor/urls.py
+++ UNCCtutor/UNCCtutor/UNCCtutor/urls.py
@@ -1,54 +1,12 @@
-"""
-URL configuration for UNCCtutor project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-import profile
-
-# from django.contrib import admin
-from django.urls import path
 from django.contrib import admin
-from django.http import HttpResponse
-from django.shortcuts import render
-
-
-
-
-def home(request):
-    return HttpResponse('Home page') 
-
-# path to login page
-def login(request):
-    return render(request, 'login.html')
-
-def register(request):
-    return render(request, 'login-register.html')
-
-def userProfile(request):
-    return render(request, 'user-profile.html')
-
-def tutorFinder(request):
-    return render(request, 'tutor-finder.html')
-
-
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
-    #    path('admin/', admin.site.urls),
-    
-    path('', home),
-    path('login/', login),
-    path('register/', register),
-    path('userProfile/', userProfile),
-    
-    path('tutorFinder/', tutorFinder),
+    path('admin/', admin.site.urls),
+    path('', include('base.urls')),
+    path('api/', include('base.api.urls'))
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
