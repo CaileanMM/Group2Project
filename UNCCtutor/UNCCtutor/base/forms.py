c1adadc9f0ed349eccb135ab7806f1f619733489
Cailean Matthews <matthewsmcailean@gmail.com>
2025-04-23 16:58:22 -0400

    Fixed the login issue

--- UNCCtutor/UNCCtutor/base/forms.py
+++ UNCCtutor/UNCCtutor/base/forms.py
@@ -7,7 +7,7 @@ from .models import User
 class MyUserCreationForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2', 'tutor']
+        fields = ['username', 'email', 'password1', 'password2']
 
 class UserProfileForm(ModelForm):
     class Meta:
