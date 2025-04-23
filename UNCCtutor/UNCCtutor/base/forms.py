a59a4820cddbe27c6327db96379274266717f5f2
Cailean Matthews <matthewsmcailean@gmail.com>
2025-04-23 17:02:23 -0400

    Tutor model implemented

--- UNCCtutor/UNCCtutor/base/forms.py
+++ UNCCtutor/UNCCtutor/base/forms.py
@@ -7,7 +7,7 @@ from .models import User
 class MyUserCreationForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ['username', 'email', 'password1', 'password2', 'tutor']
 
 class UserProfileForm(ModelForm):
     class Meta:
